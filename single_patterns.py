533a00227841407f888a70271d43d72019003db7
MdeFrance <michaeldefrance@gmail.com>
2024-09-07 14:00:14 +0200

    Create single_patterns.py
    Add single_patterns.py to define individual candlestick patterns for use in the Candlestick Pattern Quiz. 
    This module contains functions and data structures to represent and describe various candlestick patterns such as Doji, Bullish Marubozu, Bearish Marubozu, and more.

---
+++ single_patterns.py
@@ -0,0 +1,81 @@
+from colorama import init, Fore, Style
+single_patterns = {
+    "Hammer": [
+        f'                        ',
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'                        '
+    ],
+    "Inverted Hammer": [
+        f'                        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
+        f'                        '
+    ],
+    "Shooting Star": [
+        f'                        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'                        '
+    ],
+    "Hanging Man": [
+        f'                        ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'                        '
+    ],
+    "Doji": [
+        f'                        ',
+        f'             {Fore.YELLOW}║{Fore.RESET}            ',
+        f'             {Fore.YELLOW}║{Fore.RESET}            ',
+        f'             {Fore.YELLOW}║{Fore.RESET}            ',
+        f'        {Fore.YELLOW}|█████████|{Fore.RESET}        ',
+        f'             {Fore.YELLOW}║{Fore.RESET}            ',
+        f'             {Fore.YELLOW}║{Fore.RESET}            ',
+        f'             {Fore.YELLOW}║{Fore.RESET}            ',
+        f'                        '
+    ],
+    "Bullish Marubozu": [
+        f'                        ',
+        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
+        f'                        '
+    ],
+    "Bearish Marubozu": [
+        f'                        ',
+        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
+        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
+        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
+        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
+        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
+        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
+        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
+        f'                        '
+        f'                        '
+    ],
+}
