7dd5209d8dab75ce5751df3ddbc2d8d6d0d4b796
MdeFrance <michaeldefrance@gmail.com>
2024-09-07 21:35:05 +0200

    Update single_patterns.py

--- single_patterns.py
+++ single_patterns.py
@@ -1,7 +1,15 @@
-from colorama import init, Fore, Style
-single_patterns = {
+from colorama import init, Fore
+
+# Initialize colorama for color support in terminal
+init(autoreset=True)
+
+# DEBUG = True
+DEBUG = False
+
+patterns = {
     "Hammer": [
         f'                        ',
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
         f'            {Fore.GREEN}║{Fore.RESET}            ',
         f'            {Fore.GREEN}║{Fore.RESET}            ',
         f'            {Fore.GREEN}║{Fore.RESET}            ',
@@ -20,12 +28,14 @@ single_patterns = {
         f'            {Fore.GREEN}║{Fore.RESET}            ',
         f'            {Fore.GREEN}║{Fore.RESET}            ',
         f'            {Fore.GREEN}║{Fore.RESET}            ',
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
         f'                        '
     ],
     "Shooting Star": [
         f'                        ',
         f'        {Fore.RED}|███████|{Fore.RESET}        ',
         f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
         f'            {Fore.RED}║{Fore.RESET}            ',
         f'            {Fore.RED}║{Fore.RESET}            ',
         f'            {Fore.RED}║{Fore.RESET}            ',
@@ -40,42 +50,184 @@ single_patterns = {
         f'            {Fore.RED}║{Fore.RESET}            ',
         f'            {Fore.RED}║{Fore.RESET}            ',
         f'            {Fore.RED}║{Fore.RESET}            ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
         f'        {Fore.RED}|███████|{Fore.RESET}        ',
         f'        {Fore.RED}|███████|{Fore.RESET}        ',
         f'                        '
     ],
     "Doji": [
         f'                        ',
-        f'             {Fore.YELLOW}║{Fore.RESET}            ',
-        f'             {Fore.YELLOW}║{Fore.RESET}            ',
-        f'             {Fore.YELLOW}║{Fore.RESET}            ',
-        f'        {Fore.YELLOW}|█████████|{Fore.RESET}        ',
-        f'             {Fore.YELLOW}║{Fore.RESET}            ',
-        f'             {Fore.YELLOW}║{Fore.RESET}            ',
-        f'             {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'        {Fore.YELLOW}|███████|{Fore.RESET}        ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+
+        f'                        '
+    ],
+    "Long-legged Doji": [
+        f'                        ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'        {Fore.YELLOW}|███████|{Fore.RESET}        ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'                        '
+    ],
+    "Gravestone Doji": [
+        f'                        ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'        {Fore.YELLOW}|███████|{Fore.RESET}        ',
+        f'                        '
+    ],
+    "Dragonfly Doji": [
+        f'                        ',
+        f'        {Fore.YELLOW}|███████|{Fore.RESET}        ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+        f'            {Fore.YELLOW}║{Fore.RESET}            ',
+
         f'                        '
     ],
     "Bullish Marubozu": [
         f'                        ',
-        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
-        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
-        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
-        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
-        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
-        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
-        f'        {Fore.GREEN}|█████████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'                        '
+    ],
+    "Bullish Closing Marubozu": [
+        f'                        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
+
+
+        f'                        '
+    ],
+    "Bullish Opening Marubozu": [
+        f'                        ',
+
+        f'            {Fore.GREEN}║{Fore.RESET}            ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+        f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
         f'                        '
     ],
     "Bearish Marubozu": [
         f'                        ',
-        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
-        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
-        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
-        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
-        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
-        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
-        f'        {Fore.RED}|█████████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
         f'                        '
         f'                        '
     ],
+    "Bearish Closing Marubozu": [
+        f'                        ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'                        '
+    ],
+
+    "Bearish Opening Marubozu": [
+        f'                        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'                        '
+    ],
+    "Long Day Bearish": [
+        f'                        ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'        {Fore.RED}|███████|{Fore.RESET}        ',
+        f'            {Fore.RED}║{Fore.RESET}            ',
+        f'                        '
+    ],
+    "Long Day Bullish": [
+    f'                        ',
+    f'            {Fore.GREEN}║{Fore.RESET}            ',
+    f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+    f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+    f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+    f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+    f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+    f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+    f'            {Fore.GREEN}║{Fore.RESET}            ',
+    f'                        '
+    ],
+
+    "Short Day Bearish": [
+    f'                        ',
+    f'            {Fore.RED}║{Fore.RESET}            ',
+    f'            {Fore.RED}║{Fore.RESET}            ',
+    f'        {Fore.RED}|███████|{Fore.RESET}        ',
+    f'        {Fore.RED}|███████|{Fore.RESET}        ',
+    f'        {Fore.RED}|███████|{Fore.RESET}        ',
+    f'            {Fore.RED}║{Fore.RESET}            ',
+    f'            {Fore.RED}║{Fore.RESET}            ',
+    f'                        '
+],
+    "Short Day Bullish": [
+    f'                        ',
+    f'            {Fore.GREEN}║{Fore.RESET}            ',
+    f'            {Fore.GREEN}║{Fore.RESET}            ',
+    f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+    f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+    f'        {Fore.GREEN}|███████|{Fore.RESET}        ',
+    f'            {Fore.GREEN}║{Fore.RESET}            ',
+    f'            {Fore.GREEN}║{Fore.RESET}            ',
+    f'                        '
+],
+
 }
+
+def draw_double_patterns():
+    for pattern, art in single_patterns.items():
+        print(f'\n{pattern}:\n' + '\n'.join(art))
+
+# Call the function to display double candlestick patterns
+if __name__ == '__main__':
+    if DEBUG:
+        draw_double_patterns()
